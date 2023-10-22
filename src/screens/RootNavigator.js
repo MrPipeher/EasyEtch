d1833efba1fc813d88d0b89ad2a05c8087e2b2b7
kemo <theisaacinvestment@gmail.com>
2023-10-22 17:53:45 -0400

    updated

--- src/screens/RootNavigator.js
+++ src/screens/RootNavigator.js
@@ -13,7 +13,11 @@ const Stack = createNativeStackNavigator();
 
 const RootNavigator = () => {
   
-  const serverURL = 'http://10.0.0.70:5000' // Production: 'https://easyetch.onrender.com';
+  const serverURL = 'https://easyetch.onrender.com';
+
+  // Production: 'https://easyetch.onrender.com';
+  // Testing: 'http://10.0.0.70:5000'
+
   const [profileOwner, setProfileOwner] = useState(null);
 
   useEffect(() => {
